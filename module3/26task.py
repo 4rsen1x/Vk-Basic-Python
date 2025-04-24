9925504fd4fde0486012d142e442dafcf9f8eb81
4rsen1x <92880600+4rsen1x@users.noreply.github.com>
2025-04-24 17:11:36 +0300

    Create 26task.py

---
+++ module3/26task.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, age):
+        self._age = age if age >= 0 else 0
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        self._age = value if value >= 0 else 0
+code = []
+while data := input():
+   code.append(data)
+code = "\n".join(code)
+exec(code)
